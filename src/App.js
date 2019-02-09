665de40134e99a7b4cee4e69d8bfee3a91d730a6
Sarawut <sarawut.positwinyu@gmail.com>
2019-02-09 19:54:53 +0700

    Add progress bar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
 import React, { Component } from 'react';
-import {Form, Button, Container, Row, Col, InputGroup} from 'react-bootstrap';
+import {Form, Button, Container, InputGroup, ProgressBar, Row, Col} from 'react-bootstrap';
 import './App.css';
 
 
 class App extends Component {
   render() {
+
+    const now = 60;
+    const progressInstance = <ProgressBar now={now} label={`${now}%`} />;
+
     return (
       <div className="App">
         <header className="App-header">
-            <Container>
-                <InputGroup >
+          <Container>
+            <Row>
+              <InputGroup >
                 <Form.Control type="email" placeholder="Type what you need to get done here" />
-                  <Button variant="primary" type="submit">
-                    Add
-                  </Button>                    
-                </InputGroup>
-            </Container>
+                <Button variant="primary" type="submit">
+                  Add
+                  </Button>
+              </InputGroup>
+            </Row>
+            <Row>
+              <Col sm={12}>
+                {progressInstance}
+              </Col>              
+            </Row>            
+          </Container>
         </header>
       </div>
     );
