b6ab7ad2210a15a07db6f194051da612bc2218a2
Sarawut <sarawut.positwinyu@gmail.com>
2019-02-12 00:38:46 +0700

    update read me

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <Todo></Todo>
+          <Todo/>
         </header>
       </div>
     );
