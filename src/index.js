d78e7df64b9eef96d0d7d10b423b3158b4bb3d09
Sarawut <sarawut.positwinyu@gmail.com>
2019-02-12 19:52:46 +0700

    refactor reducer to use handle action

--- src/index.js
+++ src/index.js
@@ -4,68 +4,74 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
-import {applyMiddleware, createStore} from 'redux'
-import {Provider} from 'react-redux'
-import {createAction} from 'redux-actions';
-import logger from 'redux-logger'
+import { applyMiddleware, createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { createAction, handleActions } from 'redux-actions';
+import logger from 'redux-logger';
 import uuidv1 from 'uuid/v1';
 
 const initialState = {
-    todos:{}
-}
+  todos: {}
+};
 
 export const addTodo = createAction('ADD_TODO');
 export const updateTodo = createAction('UPDATE_TODO');
 export const deleteTodo = createAction('DELETE_TODO');
 export const toggleDone = createAction('TOGGLE_DONE');
 
-function todos(state, action) {
-    switch (action.type) {
-        case 'ADD_TODO':
-            const todoItem = { uuid: uuidv1(''), text: action.payload, isDone: false };
-            return {
-                ...state,
-                todos: Object.assign(state.todos, { [todoItem.uuid]: todoItem }),
-            }
-        case 'UPDATE_TODO':
-            {
-                const { uuid, text } = action.payload
-                const itemToUpdate = state.todos[uuid];
-                return {
-                    ...state,
-                    todos: Object.assign(state.todos, { [uuid]: Object.assign(itemToUpdate, {text}) }),
-                }
-            }
-        case 'DELETE_TODO':
-            {
-                const { uuid } = action.payload;
-                const { [uuid]: value, ...todos } = state.todos;
-                return {
-                    ...state,
-                    todos,
-                }
-            }
-        case 'TOGGLE_DONE':
-            {
-                const {uuid} = action.payload;
-                const itemToUpdate = state.todos[uuid];
-                return {
-                    ...state,
-                    todos: Object.assign(state.todos, { [uuid]: Object.assign(itemToUpdate, { isDone: !itemToUpdate.isDone }) }),
-                }                
-            }
-        default:
-            return state;
+// As object key in handleActions:
+const reducer = handleActions(
+  {
+    [addTodo]: (state, action) => {
+      const todoItem = {
+        uuid: uuidv1(''),
+        text: action.payload,
+        isDone: false
+      };
+      return {
+        ...state,
+        todos: Object.assign(state.todos, { [todoItem.uuid]: todoItem })
+      };
+    },
+    [updateTodo]: (state, action) => {
+      const { uuid, text } = action.payload;
+      const itemToUpdate = state.todos[uuid];
+      return {
+        ...state,
+        todos: Object.assign(state.todos, {
+          [uuid]: Object.assign(itemToUpdate, { text })
+        })
+      };
+    },
+    [deleteTodo]: (state, action) => {
+      const { uuid } = action.payload;
+      const { [uuid]: value, ...todos } = state.todos;
+      return {
+        ...state,
+        todos
+      };
+    },
+    [toggleDone]: (state, action) => {
+      const { uuid } = action.payload;
+      const itemToUpdate = state.todos[uuid];
+      return {
+        ...state,
+        todos: Object.assign(state.todos, {
+          [uuid]: Object.assign(itemToUpdate, { isDone: !itemToUpdate.isDone })
+        })
+      };
     }
-}
-const store = createStore(todos, initialState, applyMiddleware(logger))
+  },
+  initialState
+);
 
+const store = createStore(reducer, initialState, applyMiddleware(logger));
 
 ReactDOM.render(
-<Provider store={store}>
+  <Provider store={store}>
     <App />
-</Provider>, 
-document.getElementById('root')
+  </Provider>,
+  document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
